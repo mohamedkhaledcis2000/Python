9b099c3863a752a102dea66750e321a58292de12
mohamedkhaledcis2000 <mohamedkhaledcis2000@gmail.com>
2022-01-06 00:21:52 +0200

    first commit for python training

---
+++ pyOne.py
@@ -0,0 +1,14 @@
+print("Hello Python")
+# use one hash in python for single comment
+#useing multiple Hash for multiple lines comments
+#hi
+#hello
+#i 
+#love
+#python
+# i can prevent code from run by comments
+print("comment")#hello print
+# variable names
+name = "Mohamed" #normally
+myName="Mohamed" #Camel case
+my_name = "Mohamed" #snack case 
