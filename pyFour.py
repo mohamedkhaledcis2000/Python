2ddc363ce5775b41da48abfcd034a620cf256408
mohamedkhaledcis2000 <mohamedkhaledcis2000@gmail.com>
2022-01-11 00:13:53 +0200

    four updated commition

--- pyFour.py
+++ pyFour.py
@@ -5,5 +5,15 @@ v = input("Enter ur word")
 def removeVowel(v):
     return (re.sub('[aioueAIOUE]','',v))
 
-print(removeVowel(v))        
-    
+print(removeVowel(v))      
+################################################
+
+# character locator
+char = 'a'
+myChar= "mohamed"
+def findChar(char):
+    for i in myChar:
+        if(i==char):
+            return(i)
+
+print(findChar(char))
